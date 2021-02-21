ed5eed83ac4be031344da5ab926142573028efdb
NataliSb9 <nsainzbarroso@gmail.com>
2021-02-21 13:13:00 +0100

    library con metodos

--- library.ts
+++ library.ts
@@ -1,5 +1,6 @@
 import { Book } from "./book"
 import { Author } from "./author"
+const fs = require('fs')
 
 export class Library {
     public books: Book[];
@@ -7,13 +8,25 @@ export class Library {
     constructor(books: Book[]) {
         this.books = books;
     }
-    
+
     //Metodos
     public toString(): string {
-        let resultado: string=""
+        let resultado: string = ""
         for (let i = 0; i < this.books.length; i++) {
-            resultado += this.books[i].toString()+"\n";
+            resultado += this.books[i].toString() + "\n";
         }
         return resultado
+    }
+    public escribirEnFicheroJson(nombreFichero: string) {
+        let miLibreria = JSON.stringify(this)
+        fs.writeFileSync(nombreFichero, miLibreria)
+    }
+
+    public obtenerInstanciaLibreria(nombreFichero: string): Library {
+
+        let readFile = fs.readFileSync(nombreFichero);
+        let parseRf: Library = JSON.parse(readFile);
+        
+        return parseRf
     }
 }
